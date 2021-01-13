603db20b6010fa2a456bb0be42b96fc47807f3d5
MariaNeira <maria.neira@insolutions.pe>
2021-01-12 19:14:24 -0500

    a

--- imflatfield.py
+++ imflatfield.py
@@ -4,28 +4,16 @@ import cv2 as cv
 import numpy as np
 import argparse
 
-gamma = 0.724
-
-path = r'C:\Users\maria\Downloads\Frame1.jpg'
-
-img = cv.imread(path)
-img_original = img[:,:,2]
-
-def gammaCorrection():
-    ## [changing-contrast-brightness-gamma-correction]
+def gammaCorrection(img_original,gamma):
+    
     lookUpTable = np.empty((1,256), np.uint8)
     for i in range(256):
         lookUpTable[0,i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
 
     res = cv.LUT(img_original, lookUpTable)
-    ## [changing-contrast-brightness-gamma-correction]
 
     img_gamma_corrected = cv.hconcat([img_original, res])
     cv.imshow("Gamma correction", img_gamma_corrected)
 
-cv.namedWindow('Gamma correction')
-
-gammaCorrection()
-
-cv.waitKey(0)
+cv.waitKey()
 cv.destroyAllWindows()
